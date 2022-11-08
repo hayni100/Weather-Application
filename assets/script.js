69c9ed05d6f92409d6cbfefb9e01e5c6ff23ec94
Haein Kim <hayni100@gmail.com>
2022-11-07 17:18:30 -0800

    added html, css and js files with css and html content

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var cityNameInput= querySelector("#city-name-input");
+var searchButton= querySelector("#search-button");
+var pastSearch= querySelector("#past-search");
+var cityAndDate= querySelector("#city-date");
+var currentWeather = querySelector("#currentWeather");
+var currentTemp= querySelector("#current-temp");
+var currentWind= querySelector("#current-wind");var currentHumidity= querySelector("#current-humidity");
